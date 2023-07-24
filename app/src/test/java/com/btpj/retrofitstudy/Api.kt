226dfb172d6b6026d0f3351f99b5171ccbe636cf
LTP <1069113473@qq.com>
2023-07-24 12:04:28 +0800

    完成Retrofit源码解析

--- app/src/test/java/com/btpj/retrofitstudy/Api.kt
+++ app/src/test/java/com/btpj/retrofitstudy/Api.kt
@@ -1,5 +1,8 @@
 package com.btpj.retrofitstudy
 
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.SkipCallbackExecutor
 import retrofit2.http.GET
 
 /**
@@ -8,7 +11,15 @@ import retrofit2.http.GET
  */
 interface Api {
 
-    /** 获取首页banner数据 */
+    /** 获取首页banner数据（协程方式返回无包装的实体） */
     @GET("banner/json")
     suspend fun getBanner(): ApiResponse<List<Banner>>
+
+    /** 获取首页banner数据（使用协程返回Response包装的实体） */
+    @GET("banner/json")
+    suspend fun getBanner2(): Response<ApiResponse<List<Banner>>>
+
+    /** 获取首页banner数据（普通方式返回Call包装的实体） */
+    @GET("banner/json")
+    fun getBanner3(): Call<ApiResponse<List<Banner>>>
 }
--- app/src/test/java/com/btpj/retrofitstudy/RetrofitUnitTest.kt
+++ app/src/test/java/com/btpj/retrofitstudy/RetrofitUnitTest.kt
@@ -1,9 +1,10 @@
 package com.btpj.retrofitstudy
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -21,9 +22,32 @@ class RetrofitUnitTest {
     @Test
     fun getBanner() {
         val api = retrofit.create(Api::class.java)
+        // runBlocking启动的协程会阻塞住线程
         runBlocking {
             val response = api.getBanner()
             println(response)
+
+            val response2 = api.getBanner2()
+            println(response2.body())
         }
+
+        // Retrofit异步请求
+        api.getBanner3().enqueue(object : Callback<ApiResponse<List<Banner>>> {
+            override fun onResponse(
+                call: Call<ApiResponse<List<Banner>>>,
+                response: Response<ApiResponse<List<Banner>>>
+            ) {
+                println(response)
+            }
+
+            override fun onFailure(call: Call<ApiResponse<List<Banner>>>, t: Throwable) {
+            }
+        })
+        // 异步请求需要阻塞住主线程以等待请求完毕
+        Thread.sleep(3000)
+
+        // Retrofit同步请求
+        val response = api.getBanner3().execute()
+        println(response)
     }
 }
